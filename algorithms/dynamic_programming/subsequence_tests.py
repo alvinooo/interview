b34d4fd9a508b8333242b3dd910486280b0861a1
Alvin Heng <alvin.heng@teradata.com>
2018-04-24 13:47:47 -0700

    TODO backtrack lcs

--- algorithms/dynamic_programming/subsequence_tests.py
+++ algorithms/dynamic_programming/subsequence_tests.py
@@ -1,8 +1,8 @@
 from subsequence import *
 
-def test_longest_increasing_length():
-	assert 4 == longest_increasing_length([10, 9, 2, 5, 3, 7, 101, 18])
+def test_longest_increasing_subsequence():
+	assert [2, 3, 7, 101] == longest_increasing_subsequence([10, 9, 2, 5, 3, 7, 101, 18])
 
 def test_longest_common_length():
-	assert 3 == longest_common_length("ABCDGH", "AEDFHR")
-	assert 4 == longest_common_length("AGGTAB", "GXTXAYB")
+	assert "ADH" == ''.join(longest_common_subsequence("ABCDGH", "AEDFHR"))
+	assert "GTAB" == ''.join(longest_common_subsequence("AGGTAB", "GXTXAYB"))
